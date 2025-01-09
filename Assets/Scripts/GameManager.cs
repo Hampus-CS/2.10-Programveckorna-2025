28683e8caf05a7e8ad5b4eb0911002c6392a2da7
Hampus-CS <150423568+Hampus-CS@users.noreply.github.com>
2025-01-09 22:11:48 +0100

    Stockpile changes + Buttons.cs changes
    #StockpileManager.cs
    - Added stockpile list and other things for a start, maybe more things needed at a later date.
    
    #Buttons.cs
    - Changed skilltree submenus to use interface for easier making and management, also added som code for Stockpile UI among other things.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,7 +6,7 @@ public class GameManager : MonoBehaviour
     [Header("Important Values")]
     [SerializeField] private int commandPower;
     [SerializeField] private int manPower;
-    [SerializeField] private int scrap;
+    [SerializeField] private int scrap = 1000;
 
     private void Awake()
     {
@@ -24,4 +24,20 @@ public class GameManager : MonoBehaviour
     {
         
     }
+
+    public bool TrySpendScrap(int amount)
+    {
+        if (scrap >= amount)
+        {
+            scrap -= amount;
+            Debug.Log($"Scrap spent: {amount}. Remaining scrap: {scrap}");
+            return true;
+        }
+        Debug.Log("Not enough scrap.");
+        return false;
+    }
+
+    public int GetScrap() => scrap; // Getter om du vill visa scrap i UI, borde fungera, har inte testat! Halft hjärndöd när jag la till detta. ;)
+
+
 }
