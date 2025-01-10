626f9696dd6161202a72d8ee4e81404e14718c9b
MaryamMMKB <154224825+MaryamMMKB@users.noreply.github.com>
2025-01-10 16:24:21 +0100

    Weapon upgrades
    wdwd

--- Assets/Maryam/Scripts/GameManager1.cs
+++ Assets/Maryam/Scripts/GameManager1.cs
@@ -1,26 +1,76 @@
 using UnityEngine;
 
+
+
 public class GameManager1 : MonoBehaviour
 {
-    
-
-     [System.Serializable]
+    [System.Serializable]
     public class Weapon
     {
-        
-       [SerializeField] public GameObject bulletPrefab; //prefab for bullets
+        public GameObject bulletPrefab; // Bullet prefab associated with this weapon
         public int damage = 10;
         public int range = 5;
 
-        public Weapon(GameObject bullet, int damage, int range)
+        public Weapon(GameObject bulletPrefab, int damage, int range)
         {
-            this.bulletPrefab = bullet;
+            this.bulletPrefab = bulletPrefab;
             this.damage = damage;
             this.range = range;
         }
-
-
     }
-    
-    
+
+    public Weapon[] weapons; // Array of weapons
+
+    public ShopUIManager shopUIManager; // Reference to ShopUIManager
+
+    // Method to find a weapon based on the tag of the image
+    public Weapon GetWeaponFromTag(RectTransform image)
+    {
+        string imageTag = image.tag; // Get the tag of the current image
+        foreach (var weapon in weapons)
+        {
+            if (weapon.bulletPrefab != null && weapon.bulletPrefab.CompareTag(imageTag))
+            {
+                return weapon;
+            }
+        }
+
+        return null; // No matching weapon found
+    }
+
+    // Method to upgrade the damage of the weapon currently in the center of the shop
+    public void UpgradeCurrentWeaponDamage(int amount)
+    {
+        RectTransform currentImage = shopUIManager.GetCurrentImage();
+        Weapon weapon = GetWeaponFromTag(currentImage);
+
+        if (weapon != null)
+        {
+            // Upgrade the weapon's damage directly
+            weapon.damage += amount;
+            Debug.Log("Weapon's damage increased by " + amount + ". New damage: " + weapon.damage);
+        }
+        else
+        {
+            Debug.LogWarning("No weapon associated with the current image.");
+        }
+    }
+
+    // Method to upgrade the range of the weapon currently in the center of the shop
+    public void UpgradeCurrentWeaponRange(int amount)
+    {
+        RectTransform currentImage = shopUIManager.GetCurrentImage();
+        Weapon weapon = GetWeaponFromTag(currentImage);
+
+        if (weapon != null)
+        {
+            // Upgrade the weapon's range directly
+            weapon.range += amount;
+            Debug.Log("Weapon's range increased by " + amount + ". New range: " + weapon.range);
+        }
+        else
+        {
+            Debug.LogWarning("No weapon associated with the current image.");
+        }
+    }
 }
--- Assets/Maryam/Scripts/Weapon.cs
+++ Assets/Maryam/Scripts/Weapon.cs
@@ -4,8 +4,22 @@ using UnityEngine;
 public class Weapon : MonoBehaviour
 {
     public GameManager1.Weapon weapon; //reference to the weapon class form gamemanager
-    
     public Transform firePoint; //where bullets spawn
+
+    private int damage = 10; //set to private so weapons stats can ony be modified from weapon gameobject or weapon script
+    private int range = 5;// same as damage
+   
+   
+
+    private void Start()
+    {
+        if(weapon != null)
+        {
+            damage = weapon.damage; //set damage from GameManager1
+            range = weapon.range;  //set range from GameManager1
+        }
+    }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
@@ -13,28 +27,54 @@ public class Weapon : MonoBehaviour
             Shoot();
         }
     }
-    void Shoot()
+    public void Shoot()
     {
-        if (weapon != null && weapon.bulletPrefab != null)
+        if (weapon != null || weapon.bulletPrefab == null)
         {
-            //spawns the bullets
-           GameObject bulletInstance = Instantiate(
-               weapon.bulletPrefab,
-               firePoint.position, 
-               firePoint.rotation
-                
-            );
+            Debug.LogWarning("weapon or bullet prefab is not assigned");
+            return;
+        }
+        
+          //spawns the bullets
+          GameObject bulletInstance = Instantiate(
+          weapon.bulletPrefab,
+          firePoint.position, 
+          firePoint.rotation
+          );
+
             // here bullets damaga and range get decided
             Bullet bulletScript = bulletInstance.GetComponent<Bullet>();
-            if (bulletScript != null) { 
-            bulletScript.damage = weapon.damage; //bullets damage is equal to weapon damage
-            bulletScript.range = weapon.range; //bullets range is equal to weapons range
+
+            if (bulletScript != null)
+            { 
+            bulletScript.damage = damage; //bullets damage is equal to weapon damage
+            bulletScript.range = range; //bullets range is equal to weapons range
             }
             else
             {
-                Debug.LogWarning("Weapon or bullet prefab is not assigned");
+                Debug.LogWarning("Bullet prefab is not assigned");
             }
-        }
+          
+    }
+    public int GetDamage()//int to acces weapons damage
+    {
+        return damage;
+    }
+    public int GetRange()//int to acces weapons damage
+    {
+        return range;
+    }
+    public void UpgradeDamage(int amount) //void for uppgrading weapons damage in shop
+    {
+
+        damage += amount;
+        Debug.Log("Weapons damage increased by " + amount + ". New damage: " + amount + damage);
+    }
+    public void UpgradeRange(int amount)//void to uppgrade weapons range in shop
+    {
+
+        range += amount;
+        Debug.Log("Weapons range increased by " + amount + ". New range: " + amount + range);
     }
 }
 
